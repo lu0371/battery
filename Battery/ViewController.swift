b4ab3a901dd663ddc8f0cc0d7e16e508ecb17297
Steve Trease <steve@silver.trease.eu>
2015-06-30 09:07:46 +0100

    Setup

---
+++ Battery/ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  Battery
+//
+//  Created by Steve Trease on 10/06/2015.
+//  Copyright © 2015 Steve Trease. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var batteryLevel: UILabel!
+    @IBOutlet var chargeStatus: UILabel!
+    
+    var backgroundTaskIdentifier: UIBackgroundTaskIdentifier?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("starting")
+        
+        // set initial value
+        batteryLevelChanged()
+        
+        // set to update labels on battery status change notifications (only works in foreground)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "batteryLevelChanged", name: UIDeviceBatteryLevelDidChangeNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "batteryLevelChanged", name: UIDeviceBatteryStateDidChangeNotification, object: nil)
+        
+        // run a background task every fifteen minutes to call batteryLevelChanged
+        backgroundTaskIdentifier = UIApplication.sharedApplication().beginBackgroundTaskWithExpirationHandler({
+            UIApplication.sharedApplication().endBackgroundTask(self.backgroundTaskIdentifier!)
+        })
+        _ = NSTimer.scheduledTimerWithTimeInterval(15 * 60.09, target: self, selector: "batteryLevelChanged", userInfo: nil, repeats: true)
+        
+        // call batteryLevelChanged once per second when in foreground
+        // _ = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("batteryLevelChanged"), userInfo: nil, repeats: true)
+    }
+    
+    func batteryLevelChanged() {
+    
+        let formatter =  NSNumberFormatter()
+        formatter.numberStyle = .PercentStyle
+        
+        batteryLevel.text = formatter.stringFromNumber(UIDevice.currentDevice().batteryLevel)
+        switch UIDevice.currentDevice().batteryState {
+        case UIDeviceBatteryState.Unknown:
+            chargeStatus.text = "Unknown"
+        case UIDeviceBatteryState.Unplugged:
+            chargeStatus.text = "Unplugged"
+        case UIDeviceBatteryState.Charging:
+            chargeStatus.text = "Charging"
+        case UIDeviceBatteryState.Full:
+            chargeStatus.text = "Full"
+        }
+        
+        print("battery status change: " + chargeStatus.text! + " " + batteryLevel.text!)
+
+        let request = NSMutableURLRequest(URL: NSURL(string: "http://www.trease.eu/ibeacon/battery/")!)
+        request.HTTPMethod = "POST"
+        var bodyData = "&device=\(UIDevice.currentDevice().name)"
+        bodyData += "&batterystate=" + chargeStatus.text!
+        bodyData += "&batterylevel=" + batteryLevel.text!
+        request.HTTPBody = bodyData.dataUsingEncoding(NSUTF8StringEncoding)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            let x = response as? NSHTTPURLResponse
+            print ("status code \(x?.statusCode)")
+        }
+        task!.resume()
+     }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
