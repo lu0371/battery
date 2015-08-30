a737b7836e35edfd081b2c1100d7a5e07dbee303
Steve <stevetrease@gmail.com>
2015-08-30 17:11:14 +0100

    Not much really.

--- Battery/AppDelegate.swift
+++ Battery/AppDelegate.swift
@@ -13,50 +13,6 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
-    
-    func getIFAddresses() -> [String] {
-        var addresses = [String]()
-        
-        // Get list of all interfaces on the local machine:
-        var ifaddr : UnsafeMutablePointer<ifaddrs> = nil
-        if getifaddrs(&ifaddr) == 0 {
-            
-            // For each interface ...
-            for (var ptr = ifaddr; ptr != nil; ptr = ptr.memory.ifa_next) {
-                let flags = Int32(ptr.memory.ifa_flags)
-                var addr = ptr.memory.ifa_addr.memory
-                // var up = ptr.memory.ifa_data.
-                
-                // Check for running IPv4, IPv6 interfaces. Skip the loopback interface.
-                if (flags & (IFF_UP|IFF_RUNNING|IFF_LOOPBACK)) == (IFF_UP|IFF_RUNNING) {
-                    if addr.sa_family == UInt8(AF_INET) || addr.sa_family == UInt8(AF_INET6) {
-                        
-                        // Convert interface address to a human readable string:
-                        var hostname = [CChar](count: Int(NI_MAXHOST), repeatedValue: 0)
-                        if (getnameinfo(&addr, socklen_t(addr.sa_len), &hostname, socklen_t(hostname.count),
-                            nil, socklen_t(0), NI_NUMERICHOST) == 0) {
-                                if let address = String.fromCString(hostname) {
-                                    addresses.append(address);
-                                }
-                        }
-                    }
-                }
-            }
-            freeifaddrs(ifaddr)
-        }
-        
-        return addresses
-    }
-    
-    func networkUsage () {
-        print ("networkUsage")
-        
-        print (getIFAddresses());
-    }
-    
-    
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
@@ -70,12 +26,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         
         // UIApplication.sharedApplication().setMinimumBackgroundFetchInterval(UIApplicationBackgroundFetchIntervalMinimum)
-        // every 15 minutes
-        UIApplication.sharedApplication().setMinimumBackgroundFetchInterval(15 * 60)
+        // every 5 minutes
+        UIApplication.sharedApplication().setMinimumBackgroundFetchInterval(5 * 60)
         
         UIDevice.currentDevice().batteryMonitoringEnabled = true
-        
-        networkUsage()
 
         return true
     }
